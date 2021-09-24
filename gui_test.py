6952ba2e14ea41b14e58361da992fdd4ea2b4a48
Simon Xu <13837836+simjxu@users.noreply.github.com>
2021-09-23 21:30:51 -0700

    working image move overlapping with layout

--- gui_test.py
+++ gui_test.py
@@ -16,30 +16,36 @@ my_canvas = Canvas(root, width=w, height=h, bg="white")
 my_canvas.pack(fill=BOTH, expand=True)
 
 # Add the image to the canvas
-# Add the blue circle to the canvas
-blue_circle = PhotoImage(file="./images/bluecircle.png")
-my_canvas.create_image(100,100, image=blue_circle, anchor='nw')
 
-# # Puts the image of the layout into the canvas and resizes it based upon the window size
-# def resize_image(e):
-#     global image, resized, image2
-#     # open image to resize it
-#     image = Image.open("./images/layout.png")
-#     # resize the image with width and height of root
-#     resized = image.resize((e.width, e.height), Image.ANTIALIAS)
 
-#     image2 = ImageTk.PhotoImage(resized)
-#     my_canvas.create_image(0,0, image=image2, anchor='nw')
+# Puts the image of the layout into the canvas and resizes it based upon the window size
+def resize_window_cb(e):
+    # Resize the floorplan layout
+    global layout, layout_resized, layout2
+    # open image to resize it
+    layout = Image.open("./images/layout.png")
+    # resize the image with width and height of root
+    layout_resized = layout.resize((e.width, e.height), Image.ANTIALIAS)
 
+    layout2 = ImageTk.PhotoImage(layout_resized)
+    my_canvas.create_image(0,0, image=layout2, anchor='nw')
+
+
+    # Add the blue circle to the canvas, also resize, and reposition
+    global blue_circle
+    blue_circle = PhotoImage(file="./images/bluecircle.png")
+    blue_circle = blue_circle.subsample(10)
+    my_canvas.create_image(100,100, image=blue_circle, anchor='nw')
 
 
 def move(e):
     global blue_circle
     blue_circle = PhotoImage(file="./images/bluecircle.png")
+    blue_circle = blue_circle.subsample(10)     # Since we have to copy these lines so frequently, recommend we create a class or function
     blue_circle_img = my_canvas.create_image(e.x,e.y, image=blue_circle)
     
 
-    my_label.config(text="Coordinate: x:" + str(e.x) + " " + " y:" + str(e.y))
+#     my_label.config(text="Coordinate: x:" + str(e.x) + " " + " y:" + str(e.y))
 
 
 my_label = Label(root, text="")
@@ -47,6 +53,6 @@ my_label.pack(pady=20)
 
 # Button of mouse tracking
 my_canvas.bind('<B1-Motion>', move)
-# my_canvas.bind("<Configure>", resize_image)
+my_canvas.bind("<Configure>", resize_window_cb)
 
 root.mainloop()
