2f99da1b9e2644d79fb7e2417ab1b1ee35d41e6d
Simon Xu <13837836+simjxu@users.noreply.github.com>
2021-10-13 11:57:11 -0700

    See sandbox.py appended circle working. Only one moves though

--- Sandbox/sandbox2.py
+++ Sandbox/sandbox2.py
@@ -1,114 +1,203 @@
+
+# Don't go off of pixels, first find the size of the window and then plaste the image based upon locatino
 import tkinter as tk
-from tkinter import ttk
 from PIL import ImageTk, Image
 import os
+import math
+import calendar
+import datetime
+
+START_MONTH = 10 # Month to begin
+START_YEAR = 2021   # Associated year
+END_MONTH = 4  # Month to end
+END_YEAR = 2022     # Associated year
+
+# Need to input the values to ensure the columns and canvas sizes are correct
+_NUMCOLS = 8
+_NUMROWS = 3
+_MINSIZE = 100
+
+BUILDS = ["SYSTEM", "EVT"]
+
+# Add the _create_circle function ot the Canvas function, makes it easier to create a circle
+def _create_circle(self, x, y, r, **kwargs):
+    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+tk.Canvas.create_circle = _create_circle
+
+class Timeline(tk.Canvas):
+	MARKER_RADIUS = 6 # All marker radii will be the same
+
+	def __init__(self, parent, **kwargs):
+
+		super().__init__(parent)
+
+        # Get number of days and months
+		self.num_days = kwargs['num_days']
+		self.num_months = kwargs['num_months']
+        
+        # Create canvas that covers the the entire row
+		self.canvas = tk.Canvas.__init__(self)
+		self.grid(column=kwargs['column'], row=kwargs['row'], \
+            columnspan=kwargs['columnspan'], rowspan=kwargs['rowspan'])
+
+        # TODO: Update the height to be shorter, and move the marker appropriately.
+		self.configure(width=100*(self.num_months), height=100)
+        
+		# Add the circle plus accompanying text
+		global my_circle
+		my_circle = []
+		my_circle.append(self.create_circle(50, 50, self.MARKER_RADIUS, fill="blue", \
+            outline="white", width=2))
+		my_circle.append(self.create_circle(75, 75, self.MARKER_RADIUS, fill="blue", \
+            outline="white", width=2))
+		# global my_circle2
+		# my_circle2 = self.create_circle(50, 50, self.MARKER_RADIUS, fill="blue", \
+        #     outline="white", width=2)
+
+		global my_text
+		my_text = self.create_text(50, 50, text="hello", fill='white')
+		self.bind('<B1-Motion>', self.move_cb)
+
+	def update_date(self, x):
+        # Create the text that goes under the marker indicating the date
+        # x is the position that the mouse moves the marker to
+        # 1. Divide the pixel count by 100
+        # 2. Round down to integer
+        # 3. Map the integer to the month Start Month + integer
+
+		month_iter = math.floor(x/100)
+		month_num = START_MONTH+month_iter
+		month_num = month_num if month_num <= 12 else month_num-12  # Rollover to January
+		return str(month_num) + "/" + \
+            str(math.ceil((x+1-100*month_iter)/100*self.num_days[month_iter]))
+        # TODO: Set bounds so that the marker doesn't go out of bounds
+
+    
+	# Now we have to indicate which circle got clicked
+	def move_cb(self, e):
+        # Callback when moving the marker
+		circle_coords = self.coords(my_circle[1])      # Returns top left and bottom right corners
+		x0 = circle_coords[0]   # currently unused, go off of the mouse position
+		y0 = circle_coords[1]
+		x1 = circle_coords[2]   # currently unused, go off of the mouse position
+		y1 = circle_coords[3]
+		self.coords(my_circle[1], e.x-self.MARKER_RADIUS, y0, e.x+self.MARKER_RADIUS, y1)
+
+        # Update label position and date
+		self.coords(my_text, e.x, 60)
+		self.tag_raise(my_text)            # Bring the text to the front, otherwise it is behind the circle.
+		self.itemconfig(my_text, text=str(self.update_date(e.x)))
+
+    # BELOW ITEMS ARE NOT COMPLETE
+	def resize_window_cb(self, e):
+		global layout, layout_resized, layout2
+		layout = Image.open("./images/layout.png")
+		layout_resized = layout.resize((e.width, e.height), Image.ANTIALIAS)
+		layout2 = ImageTk.PhotoImage(layout_resized)
+		self.create_image(0,0, image=layout2, anchor='nw')
+    
+class Marker(tk.Canvas):
+    MARKER_RADIUS = 6 # All marker radii will be the same
+    def __init__(self):
+        # Add the circle plus accompanying text
+        global my_circle
+        my_circle = self.create_circle(50, 50, self.MARKER_RADIUS, fill="blue", \
+            outline="white", width=2)
+
+        global my_text
+        my_text = self.create_text(50, 50, text="hello", fill='white')
+
+    def move_cb(self, e):
+        # Callback when moving the marker
+        circle_coords = self.coords(my_circle)      # Returns top left and bottom right corners
+        x0 = circle_coords[0]   # currently unused, go off of the mouse position
+        y0 = circle_coords[1]
+        x1 = circle_coords[2]   # currently unused, go off of the mouse position
+        y1 = circle_coords[3]
+        self.coords(my_circle, e.x-self.MARKER_RADIUS, y0, e.x+self.MARKER_RADIUS, y1)
+
+        # Update label position and date
+        self.coords(my_text, e.x, 60)
+        self.tag_raise(my_text)            # Bring the text to the front, otherwise it is behind the circle.
+        self.itemconfig(my_text, text=str(self.update_date(e.x)))
+    
+    
+
+class MainApplication(tk.Frame):
+    _NUMBER_OF_DAYS = []
+    _NUMBER_OF_MONTHS = 0
+    
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+        # Configure size of the grid on root
+        for i in range(_NUMCOLS):
+            root.columnconfigure(i, minsize=_MINSIZE)
+        for i in range(_NUMROWS):
+            root.columnconfigure(i, minsize=_MINSIZE)
+
+        # Create top row of months, get array of days, set column/rowspan
+        self._NUMBER_OF_MONTHS = self.get_num_months()
+        self._NUMBER_OF_DAYS = self.create_months()
+
+        # # Try putting 2 timelines on -----FIX: need to create an array of Timelines
+        firsttimeline = Timeline(self, column=1, row=1, columnspan=_NUMCOLS-1, rowspan=1, \
+            num_days=self._NUMBER_OF_DAYS, num_months=self._NUMBER_OF_MONTHS)
+        
+
+        secondtimeline = Timeline(self, column=1, row=2, columnspan=_NUMCOLS-1, rowspan=1, \
+            num_days=self._NUMBER_OF_DAYS, num_months=self._NUMBER_OF_MONTHS)
+        # secondtimeline.bind('<B1-Motion>', secondtimeline.move_cb)
+
+        # Builds going vertical on the left side
+        self.build1 = tk.Label(parent, text="System")
+        self.build1.grid(column=0, row=1, padx=10, pady=0)
+
+        self.build1 = tk.Label(parent, text="EVT")
+        self.build1.grid(column=0, row=2, padx=10, pady=0)
+
+    # Function to count number of months based upon inputs
+    def get_num_months(self):
+        start_date = datetime.datetime(START_YEAR,START_MONTH,1)
+        end_date = datetime.datetime(END_YEAR, END_MONTH, 1)
+        return (end_date.year - start_date.year) * 12 \
+            + (end_date.month - start_date.month) + 1
+
+    # Function to get the months and create array of days
+    def create_months(self):
+        monthdays_arr = []
+        num_months = self._NUMBER_OF_MONTHS
+
+        # Create the month labels on the first row
+        label_arr = []
+        year = START_YEAR
+        month = START_MONTH
+        for i in range(num_months):
+            # Account for months rollover at end of year
+            if month==13:
+                month = 1
+                year += 1
+
+            label_arr.append(tk.Label(root, \
+                text="  "+calendar.month_abbr[month]))
+            label_arr[i].grid(column=i+1, row=0, padx=0, pady=0)
+            monthdays_arr.append(calendar.monthrange(year,month)[1])
+            month += 1
+
+        return monthdays_arr
+
+    def create_builds(self):
+        a = 0
+        return a
+
+# class Legend(tk.Frame):
+#     def __init__(self, parent, *args, **kwargs):
 
 
-# root window
-root = tk.Tk()
-root.geometry("1200x800")
-root.title('Floor Plan')
 
-# configure the grid
-# root.columnconfigure(0, weight=1)
-# root.columnconfigure(1, weight=1)
-root.grid_columnconfigure(0, minsize=100, weight=1, uniform="foo")
-root.grid_columnconfigure(1, minsize=100, weight=1, uniform="foo")
-root.grid_columnconfigure(2, minsize=100, weight=1, uniform="foo")
-root.grid_columnconfigure(3, minsize=100, weight=1, uniform="foo")
-root.grid_columnconfigure(4, minsize=100, weight=1, uniform="foo")
-root.grid_columnconfigure(5, minsize=100, weight=1, uniform="foo")
-
-
-
-
-# # Months going horizontal on top
-# month1 = tk.Label(root, text="  Jan")
-# month1.grid(column=1, row=0, padx=0, pady=0,)
-
-# month2 = tk.Label(root, text="  Feb")
-# month2.grid(column=2, row=0, padx=0, pady=0,)
-
-# month3 = tk.Label(root, text="  Mar")
-# month3.grid(column=3, row=0, padx=0, pady=0,)
-
-# Same thing as above with an array
-label_arr = []
-label_arr.append(tk.Label(root, text="  Jan"))
-label_arr[0].grid(column=1, row=0, padx=0, pady=0)
-
-label_arr.append(tk.Label(root, text="  Feb"))
-label_arr[1].grid(column=2, row=0, padx=0, pady=0)
-
-label_arr.append(tk.Label(root, text="  Mar"))
-label_arr[2].grid(column=3, row=0, padx=0, pady=0,)
-
-label_arr.append(tk.Label(root, text="  Apr"))
-label_arr[3].grid(column=4, row=0, padx=0, pady=0,)
-
-# Builds going vertical on the left side
-build1 = tk.Label(root, text="System")
-build1.grid(column=0, row=1)
-
-build1 = tk.Label(root, text="EVT")
-build1.grid(column=0, row=2)
-
-
-
-
-# Create the Canvas
-
-my_canvas = tk.Canvas(root)
-my_canvas.grid(column=1, row=1, columnspan=20)
-
-my_canvas2 = tk.Canvas(root)
-my_canvas2.grid(column=1, row=2, columnspan=20)
-
-# Add the circle into the window
-global blue_circle
-blue_circle = tk.PhotoImage(file="./images/bluecircle.png")
-blue_circle = blue_circle.subsample(30)
-my_canvas.create_image(100,100,image=blue_circle)
-
-global blue_circle2
-blue_circle2 = tk.PhotoImage(file="./images/bluecircle.png")
-blue_circle2 = blue_circle2.subsample(30)
-my_canvas2.create_image(100,100,image=blue_circle2)
-
-# Add the text
-global my_text
-my_text = my_canvas.create_text(100,100, text="hello", fill='white')
-
-global my_text2
-my_text2 = my_canvas2.create_text(100,100, text="hello", fill='white')
-
-def move(e):
-	global blue_circle
-	blue_circle = tk.PhotoImage(file="./images/bluecircle.png")
-	blue_circle = blue_circle.subsample(30)     # Since we have to copy these lines so frequently, recommend we create a class or function
-	blue_circle_img = my_canvas.create_image(e.x,100, image=blue_circle) # now locaked to the 100 pixel mark
-
-	# Update label position and coordinates
-	my_canvas.coords(my_text, e.x, 120)
-	my_canvas.tag_raise(my_text)            # Bring the text to the front, otherwise it is behind the circle.
-	my_canvas.itemconfig(my_text, text=str(e.x) + "," + str(e.y))
-
-	
-
-def move2(e):
-	global blue_circle2
-	blue_circle2 = tk.PhotoImage(file="./images/bluecircle.png")
-	blue_circle2 = blue_circle2.subsample(30)     # Since we have to copy these lines so frequently, recommend we create a class or function
-	blue_circle_img2 = my_canvas2.create_image(e.x,100, image=blue_circle2) # now locaked to the 100 pixel mark
-
-	# Update label position and coordinates
-	my_canvas2.coords(my_text2, e.x, 120)
-	my_canvas2.tag_raise(my_text2)            # Bring the text to the front, otherwise it is behind the circle.
-	my_canvas2.itemconfig(my_text2, text=str(e.x) + "," + str(e.y))
-
-
-my_canvas.bind('<B1-Motion>', move)
-my_canvas2.bind('<B1-Motion>', move2)
-
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("800x600")
+    MainApplication(root)
+    root.mainloop()
