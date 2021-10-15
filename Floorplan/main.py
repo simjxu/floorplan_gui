fd5a23ed9c839afa337d887605ff16002b5afc57
Simon Xu <13837836+simjxu@users.noreply.github.com>
2021-10-14 22:54:38 -0700

    working labels/dates pulled from YAML

--- Floorplan/main.py
+++ Floorplan/main.py
@@ -4,13 +4,13 @@ import math
 import calendar
 import datetime
 
-START_MONTH = 10 # Month to begin
+START_MONTH = 1 # Month to begin
 START_YEAR = 2021   # Associated year
-END_MONTH = 4  # Month to end
-END_YEAR = 2022     # Associated year
+END_MONTH = 5  # Month to end
+END_YEAR = 2021     # Associated year
 
 # Need to input the values to ensure the columns and canvas sizes are correct
-_NUMCOLS = 8
+_NUMCOLS = 6
 _NUMROWS = 3
 _MINSIZE = 100
 
@@ -18,165 +18,167 @@ BUILDS = ["SYSTEM", "EVT"]
 
 # Add the _create_circle function ot the Canvas function, makes it easier to create a circle
 def _create_circle(self, x, y, r, **kwargs):
-    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+	return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 tk.Canvas.create_circle = _create_circle
 class Timeline(tk.Frame):
     
-    MARKER_RADIUS = 8 # All marker radii will be the same
-    marker_ypos = _MINSIZE/2+MARKER_RADIUS/2
-    array = [(25,marker_ypos),(50,marker_ypos),(75,marker_ypos)]
+	MARKER_RADIUS = 8 # All marker radii will be the same
+	marker_ypos = _MINSIZE/2+MARKER_RADIUS/2
 
-    def __init__(self, parent, **kwargs):
-        self.num_days = kwargs['num_days']
-        self.num_months = kwargs['num_months']
+	# This needs to move into the __init__ function, from reading from the yaml
+	array = [(25,marker_ypos),(50,marker_ypos),(75,marker_ypos)]
 
-        self.canvas = tk.Canvas(parent)
-        # self.canvas.pack()
-        self.canvas.grid(column=kwargs['column'], row=kwargs['row'], rowspan=kwargs['rowspan'], \
-            columnspan=kwargs['columnspan'])
-        self.canvas.configure(width=_MINSIZE*(self.num_months), height=_MINSIZE, bg='green')
+	def __init__(self, parent, **kwargs):
+		self.num_days = kwargs['num_days']
+		self.num_months = kwargs['num_months']
 
-        # to keep all IDs and its start position
-        self.ovals = {}
-        self.texts = {}
+		self.canvas = tk.Canvas(parent)
+		# self.canvas.pack()
+		self.canvas.grid(column=kwargs['column'], row=kwargs['row'], rowspan=kwargs['rowspan'], \
+				columnspan=kwargs['columnspan'])
+		self.canvas.configure(width=_MINSIZE*(self.num_months), height=_MINSIZE, bg='green')
 
-        # Create markers for every item in the array
-        for item in self.array:
-            # create oval and get its ID
-            item_id = self.canvas.create_circle(item[0], item[1], self.MARKER_RADIUS, \
-                fill='blue', outline='white', tags='id')
-            # remember ID and its start position
-            self.ovals[item_id] = item
+		# to keep all IDs and its start position
+		self.ovals = {}
+		self.texts = {}
 
-            # Create texts and store the text tag id for reference during move
-            self.texts[item_id] = self.canvas.create_text(item[0], item[1]+2*self.MARKER_RADIUS, \
-                text="hello", fill='white')
+		# Create markers for every item in the array
+		for item in self.array:
+			# create oval and get its ID
+			item_id = self.canvas.create_circle(item[0], item[1], self.MARKER_RADIUS, \
+				fill='blue', outline='white', tags='id')
+			# remember ID and its start position
+			self.ovals[item_id] = item
 
-        self.canvas.tag_bind('id', '<ButtonPress-1>', self.start_move)
-        self.canvas.tag_bind('id', '<B1-Motion>', self.move)
-        self.canvas.tag_bind('id', '<ButtonRelease-1>', self.stop_move)
+			# Create texts and store the text tag id for reference during move
+			self.texts[item_id] = self.canvas.create_text(item[0], item[1]+2*self.MARKER_RADIUS, \
+				text="hello", fill='white')
 
-        # # to remember selected item
-        # self.selected = None
+			self.canvas.tag_bind('id', '<ButtonPress-1>', self.start_move)
+			self.canvas.tag_bind('id', '<B1-Motion>', self.move)
+			self.canvas.tag_bind('id', '<ButtonRelease-1>', self.stop_move)
 
-    def update_date(self, x):
-        # Create the text that goes under the marker indicating the date
-        # x is the position that the mouse moves the marker to
-        # 1. Divide the pixel count by 100
-        # 2. Round down to integer
-        # 3. Map the integer to the month Start Month + integer
+			# # to remember selected item
+			# self.selected = None
 
-        month_iter = math.floor(x/100)
-        month_num = START_MONTH+month_iter
-        month_num = month_num if month_num <= 12 else month_num-12  # Rollover to January
+	def update_date(self, x):
+		# Create the text that goes under the marker indicating the date
+		# x is the position that the mouse moves the marker to
+		# 1. Divide the pixel count by 100
+		# 2. Round down to integer
+		# 3. Map the integer to the month Start Month + integer
 
-        return str(month_num) + "/" + \
-            str(math.ceil((x+1-100*month_iter)/100*self.num_days[month_iter]))
-        # TODO: Set bounds so that the marker doesn't go out of bounds
+		month_iter = math.floor(x/100)
+		month_num = START_MONTH+month_iter
+		month_num = month_num if month_num <= 12 else month_num-12  # Rollover to January
 
-    def start_move(self, event):
-        # find all clicked items
-        self.selected = self.canvas.find_overlapping(event.x, event.y, event.x, event.y)
-        # get first selected item
-        self.selected = self.selected[0]
+		return str(month_num) + "/" + \
+			str(math.ceil((x+1-100*month_iter)/100*self.num_days[month_iter]))
+		# TODO: Set bounds so that the marker doesn't go out of bounds
 
-        # get selected text tag
-        self.selected_text = self.canvas.find_withtag(self.texts[self.selected])
+	def start_move(self, event):
+		# find all clicked items
+		self.selected = self.canvas.find_overlapping(event.x, event.y, event.x, event.y)
+		# get first selected item
+		self.selected = self.selected[0]
 
-    def move(self, event):
-        circle_coords = self.canvas.coords(self.selected)
-        x0 = circle_coords[0]   # currently unused, go off of the mouse position
-        y0 = circle_coords[1]
-        x1 = circle_coords[2]   # currently unused, go off of the mouse position
-        y1 = circle_coords[3]
+		# get selected text tag
+		self.selected_text = self.canvas.find_withtag(self.texts[self.selected])
 
-        # move selected item, hold y position
-        self.canvas.coords(self.selected, event.x-self.MARKER_RADIUS, \
-            y0, event.x+self.MARKER_RADIUS,y1)
+	def move(self, event):
+		circle_coords = self.canvas.coords(self.selected)
+		x0 = circle_coords[0]   # currently unused, go off of the mouse position
+		y0 = circle_coords[1]
+		x1 = circle_coords[2]   # currently unused, go off of the mouse position
+		y1 = circle_coords[3]
 
-        # Also move the label position and date
-        self.canvas.coords(self.selected_text, event.x, \
-            self.marker_ypos+2*self.MARKER_RADIUS)
-        self.canvas.tag_raise(self.selected_text)
-        self.canvas.itemconfig(self.selected_text, text=str(self.update_date(event.x)))
+		# move selected item, hold y position
+		self.canvas.coords(self.selected, event.x-self.MARKER_RADIUS, \
+				y0, event.x+self.MARKER_RADIUS,y1)
 
-    def stop_move(self, event):
-        print("stopped")
+		# Also move the label position and date
+		self.canvas.coords(self.selected_text, event.x, \
+				self.marker_ypos+2*self.MARKER_RADIUS)
+		self.canvas.tag_raise(self.selected_text)
+		self.canvas.itemconfig(self.selected_text, text=str(self.update_date(event.x)))
+
+	def stop_move(self, event):
+		print("stopped")
 
 class YAML_outputs:
-    def __init__(self):
-        print("YAML")
+	def __init__(self):
+		print("YAML")
 
 class MainApplication:
-    _NUMBER_OF_DAYS = []
-    _NUMBER_OF_MONTHS = 0
+	_NUMBER_OF_DAYS = []
+	_NUMBER_OF_MONTHS = 0
     
-    def __init__(self, parent):
-        self.mainframe = tk.Frame(parent, width=1000, height=1000)
-        self.mainframe.grid(column=0, row=0, rowspan=20, columnspan=20)
+	def __init__(self, parent):
+		self.mainframe = tk.Frame(parent, width=1000, height=1000)
+		self.mainframe.grid(column=0, row=0, rowspan=20, columnspan=20)
 
-        # Configure size of the grid on root
-        for i in range(_NUMCOLS):
-            root.columnconfigure(i, minsize=_MINSIZE)
-        for i in range(_NUMROWS):
-            root.columnconfigure(i, minsize=_MINSIZE)
+		# Configure size of the grid on root
+		for i in range(_NUMCOLS):
+			root.columnconfigure(i, minsize=_MINSIZE)
+		for i in range(_NUMROWS):
+				root.columnconfigure(i, minsize=_MINSIZE)
 
-        # Create top row of months, get array of days, set column/rowspan
-        self._NUMBER_OF_MONTHS = self.get_num_months()
-        self._NUMBER_OF_DAYS = self.create_months()
+		# Create top row of months, get array of days, set column/rowspan
+		self._NUMBER_OF_MONTHS = self.get_num_months()
+		self._NUMBER_OF_DAYS = self.create_months()
 
-        # # Try putting 2 timelines on -----FIX: need to create an array of Timelines
-        firsttimeline = Timeline(self.mainframe, column=1, row=1, columnspan=_NUMCOLS-1, rowspan=1, \
-            num_days=self._NUMBER_OF_DAYS, num_months=self._NUMBER_OF_MONTHS)
+		# # Try putting 2 timelines on -----FIX: need to create an array of Timelines
+		firsttimeline = Timeline(self.mainframe, column=1, row=1, columnspan=_NUMCOLS-1, rowspan=1, \
+			num_days=self._NUMBER_OF_DAYS, num_months=self._NUMBER_OF_MONTHS)
 
-        secondtimeline = Timeline(self.mainframe, column=1, row=2, columnspan=_NUMCOLS-1, rowspan=1, \
-            num_days=self._NUMBER_OF_DAYS, num_months=self._NUMBER_OF_MONTHS)
+		secondtimeline = Timeline(self.mainframe, column=1, row=2, columnspan=_NUMCOLS-1, rowspan=1, \
+			num_days=self._NUMBER_OF_DAYS, num_months=self._NUMBER_OF_MONTHS)
 
 
-        # Builds going vertical on the left side
-        self.build1 = tk.Label(self.mainframe, text="System")
-        self.build1.grid(column=0, row=1, padx=10, pady=0)
+		# Builds going vertical on the left side
+		self.build1 = tk.Label(self.mainframe, text="System")
+		self.build1.grid(column=0, row=1, padx=10, pady=0)
 
-        self.build1 = tk.Label(self.mainframe, text="EVT")
-        self.build1.grid(column=0, row=2, padx=10, pady=0)
+		self.build1 = tk.Label(self.mainframe, text="EVT")
+		self.build1.grid(column=0, row=2, padx=10, pady=0)
 
     # Function to count number of months based upon inputs
-    def get_num_months(self):
-        start_date = datetime.datetime(START_YEAR,START_MONTH,1)
-        end_date = datetime.datetime(END_YEAR, END_MONTH, 1)
-        return (end_date.year - start_date.year) * 12 \
-            + (end_date.month - start_date.month) + 1
+	def get_num_months(self):
+		start_date = datetime.datetime(START_YEAR,START_MONTH,1)
+		end_date = datetime.datetime(END_YEAR, END_MONTH, 1)
+		return (end_date.year - start_date.year) * 12 \
+			+ (end_date.month - start_date.month) + 1
 
     # Function to get the months and create array of days
-    def create_months(self):
-        monthdays_arr = []
-        num_months = self._NUMBER_OF_MONTHS
+	def create_months(self):
+		monthdays_arr = []
+		num_months = self._NUMBER_OF_MONTHS
 
-        # Create the month labels on the first row
-        label_arr = []
-        year = START_YEAR
-        month = START_MONTH
-        for i in range(num_months):
-            # Account for months rollover at end of year
-            if month==13:
-                month = 1
-                year += 1
+		# Create the month labels on the first row
+		label_arr = []
+		year = START_YEAR
+		month = START_MONTH
+		for i in range(num_months):
+			# Account for months rollover at end of year
+			if month==13:
+				month = 1
+				year += 1
 
-            label_arr.append(tk.Label(self.mainframe, \
-                text="  "+calendar.month_abbr[month]))
-            label_arr[i].grid(column=i+1, row=0, padx=0, pady=0)
-            monthdays_arr.append(calendar.monthrange(year,month)[1])
-            month += 1
+			label_arr.append(tk.Label(self.mainframe, \
+				text="  "+calendar.month_abbr[month]))
+			label_arr[i].grid(column=i+1, row=0, padx=0, pady=0)
+			monthdays_arr.append(calendar.monthrange(year,month)[1])
+			month += 1
 
-        return monthdays_arr
+		return monthdays_arr
 
-    def create_builds(self):
-        a = 0
-        return a
+	def create_builds(self):
+		a = 0
+		return a
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.geometry("800x600")
-    d = MainApplication(root)
-    root.mainloop()
+	root = tk.Tk()
+	root.geometry("800x600")
+	d = MainApplication(root)
+	root.mainloop()
--- Sandbox/sandbox.py
+++ Sandbox/sandbox.py
@@ -2,8 +2,84 @@
 
 import yaml
 
-with open("./Sandbox/example.yaml", "r") as stream:
-    try:
-        print(yaml.safe_load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
+class YAMLoutput:
+	TOP_LEVELS = []
+
+	# The following need to be accessed by Main App
+	BUILD_NAMES = []
+	DATE_ARRAYS = []
+	LABEL_ARRAYS = []
+	START_MONTH = 0
+	START_YEAR = 0
+	END_MONTH = 0
+	END_YEAR = 0
+
+
+	def __init__(self, parent, **kwargs):
+		with open(kwargs['file'], "r") as stream:
+			try:
+				yaml_dict = yaml.safe_load(stream)
+				# print(yaml_dict)
+			except yaml.YAMLError as exc:
+				print(exc)
+		
+		# print(yaml_dict)
+		# print(yaml_dict['Module'])
+		# print(yaml_dict['Module']['EVT'])
+		# print(yaml_dict['Module']['EVT'][0]['A'][0]['date'])
+
+		# Capture build names, the labels, and the dates
+		for key in yaml_dict:
+			# System Level
+			if key == "System" or key == "Assembly":
+				self.BUILD_NAMES.append(key)
+				syslabel_arr = []
+				sysdate_arr = []
+
+				# System Label Level
+				for keyS in yaml_dict[key]:
+					syslabel_arr.append(keyS)	
+					sysdate_arr.append(yaml_dict[key][keyS][0]['date'])
+				self.LABEL_ARRAYS.append(syslabel_arr)
+				self.DATE_ARRAYS.append(sysdate_arr)
+
+			else:
+				# Module Build Level
+				for bld in yaml_dict[key]:
+					self.BUILD_NAMES.append(bld)
+
+					modlabel_arr = []
+					moddate_arr = []
+
+					# Module Label Level
+					for i in range(len(yaml_dict[key][bld])):
+
+						# Moudle Date Level
+						for lbl, dateM in yaml_dict[key][bld][i].items():
+							modlabel_arr.append(lbl)
+							moddate_arr.append(dateM[0]['date'])
+					self.LABEL_ARRAYS.append(modlabel_arr)
+					self.DATE_ARRAYS.append(moddate_arr)
+
+		# print(self.BUILD_NAMES)						
+		# print(self.DATE_ARRAYS)
+		# print(self.LABEL_ARRAYS)
+
+
+
+		# for entry in yaml_dict:
+		# 	print(yaml_dict[entry])
+
+	def get_dates(selection):
+		print("placeholder")
+
+	def save_current():
+		print("placeholder")
+
+class MainApplication:
+	def __init__(self, **kwargs):
+		yamlobj = YAMLoutput(self, file=kwargs['file'])
+
+if __name__ == "__main__":
+	ymlfile = "./Sandbox/example.yaml"
+	app = MainApplication(file=ymlfile)
