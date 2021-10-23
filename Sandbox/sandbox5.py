8170ed2f55bb755d34e6f27de033b8c3ea831908
Simon Xu <13837836+simjxu@users.noreply.github.com>
2021-10-22 22:06:32 -0700

    sandbox5 working horizontal scroll

--- Sandbox/sandbox5.py
+++ Sandbox/sandbox5.py
@@ -1,3 +1,39 @@
+# import tkinter as tk
+# from tkinter import ttk
+
+# root = tk.Tk()
+
+# # Container contains the main canvas and the scrollbar
+# container = tk.Frame(root)
+
+# canvas = tk.Canvas(container)
+# scrollbar =  tk.Scrollbar(container, orient=tk.VERTICAL, command=canvas.yview)
+
+# # Frame inside the main canvas that has the scrollable area
+# scrollable_frame = tk.Frame(canvas)
+# # scrollable_frame.grid(row=0, column=0)
+
+# scrollable_frame.bind(
+#     "<Configure>",
+#     lambda e: canvas.configure(
+#         scrollregion=canvas.bbox("all")
+#     )
+# )
+
+# canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+
+# canvas.configure(yscrollcommand=scrollbar.set)
+
+# for i in range(50):
+#     ttk.Label(scrollable_frame, text="Sample scrolling label").grid(row=i, column=0)
+
+# container.grid(row=0, column=0)
+# canvas.grid(row=0, column=0)
+# scrollbar.grid(row=0, column=1, sticky=tk.NS)
+
+# root.mainloop()
+
+
 import tkinter as tk
 from tkinter import ttk
 
@@ -7,7 +43,7 @@ root = tk.Tk()
 container = tk.Frame(root)
 
 canvas = tk.Canvas(container)
-scrollbar =  tk.Scrollbar(container, orient=tk.VERTICAL, command=canvas.yview)
+scrollbar =  tk.Scrollbar(container, orient=tk.HORIZONTAL, command=canvas.xview)
 
 # Frame inside the main canvas that has the scrollable area
 scrollable_frame = tk.Frame(canvas)
@@ -22,13 +58,13 @@ scrollable_frame.bind(
 
 canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
 
-canvas.configure(yscrollcommand=scrollbar.set)
+canvas.configure(xscrollcommand=scrollbar.set)
 
 for i in range(50):
-    ttk.Label(scrollable_frame, text="Sample scrolling label").grid(row=i, column=0)
+    ttk.Label(scrollable_frame, text="Sample scrolling label").grid(row=0, column=i)
 
 container.grid(row=0, column=0)
 canvas.grid(row=0, column=0)
-scrollbar.grid(row=0, column=1, sticky=tk.NS)
+scrollbar.grid(row=1, column=0, sticky=tk.EW)
 
 root.mainloop()
